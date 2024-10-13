24ada207d7a9fff7e8c36f5e741f40ecf3da78bc
Ragad Alhaddi <hl6756@wayne.edu>
2024-10-13 02:18:43 -0400

    adding optional features

--- src/app/userprofile/page.tsx
+++ src/app/userprofile/page.tsx
@@ -8,6 +8,8 @@ export default function UserProfile() {
   const [fullname, set_fullname] = useState('');
   const [email, set_email] = useState('');
   const [city, set_city] = useState('');
+  const [phone_number, set_phone_number] = useState('');
+  const [prof_pic, set_profpic] = useState<File | null>(null);
   const router = useRouter();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -15,6 +17,8 @@ export default function UserProfile() {
     console.log('Full Name:', fullname);
     console.log('City:', city);
     console.log('Email:', email);
+    console.log('Phone Number:', phone_number);
+    console.log('Profile Picture:', prof_pic);
   };
 
   const handleLogout = () => {
@@ -23,13 +27,28 @@ export default function UserProfile() {
     router.push('/login'); // Redirect back to the login page after logging out
   };
 
+  const handlePfpUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      set_profpic(e.target.files[0]); // Set the selected profile picture
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray p-6">
-      <div className="container max-w-lg p-8 bg-black shadow-lg rounded-lg text-center">
+      <div className="container max-w-[60rem] p-12 bg-black shadow-lg rounded-lg text-center">
         <h1 className="text-4xl font-bold mb-6 text-[#f4d9a0]">Your Profile</h1>
         <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+            <label className="block text-[#f4d9a0] text-left">Profile Picture</label>
+            <input
+              type="file"
+              onChange={handlePfpUpload}
+              className="w-full p-2 border rounded"
+            />
+          </div>
+
           <div className="mb-4">
-            <label className="block text-[#f4d9a0] text-left">Name</label>
+            <label className="block text-[#f4d9a0] text-left">Name*</label>
             <input
               type="text"
               value={fullname}
@@ -42,7 +61,7 @@ export default function UserProfile() {
           <br/>
 
           <div className="mb-4">
-            <label className="block text-[#f4d9a0] text-left">City</label>
+            <label className="block text-[#f4d9a0] text-left">City*</label>
             <input
               type="text"
               value={city}
@@ -54,7 +73,7 @@ export default function UserProfile() {
           <br/>
 
           <div className="mb-4">
-            <label className="block text-[#f4d9a0] text-left">Email</label>
+            <label className="block text-[#f4d9a0] text-left">Email*</label>
             <input
               type="email"
               value={email}
@@ -62,6 +81,18 @@ export default function UserProfile() {
               placeholder="Enter your email"
               className="w-full p-2 border rounded"
               required
+            />
+          </div>
+          <br/>
+
+          <div className="mb-4">
+            <label className="block text-[#f4d9a0] text-left">Phone Number (optional)</label>
+            <input
+              type="tel"
+              value={phone_number}
+              onChange={(e) => set_phone_number(e.target.value)}
+              placeholder="Enter your phone number"
+              className="w-full p-2 border rounded"
             />
           </div>
           <br/>
@@ -79,13 +110,6 @@ export default function UserProfile() {
             className="bg-blue-500 text-white py-2 px-6 rounded-lg shadow-md hover:bg-blue-600 transition mb-4"
           >
             Go to Homepage
-          </button>
-          <br />
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 text-white py-2 px-6 rounded-lg shadow-md hover:bg-red-600 transition"
-          >
-            Logout
           </button>
         </div>
       </div>
