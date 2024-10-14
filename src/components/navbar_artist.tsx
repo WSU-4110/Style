9e251bfd3e2b0a4dacd032b3ba1886f13c200c41
Ragad Alhaddi <hl6756@wayne.edu>
2024-10-13 22:14:23 -0400

    desiging navbar and updating logout button

--- src/components/navbar_artist.tsx
+++ src/components/navbar_artist.tsx
@@ -1,19 +1,26 @@
 'use client'
 
 import React from 'react';
+//import './navigationbar.css';
 import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
   const router = useRouter();
 
+  const handlelogout = () => {
+    localStorage.removeItem('authToken');
+    alert('Logging out');
+    router.push('/');
+  }
+
   return (
     <nav>
       <ul>
         <li><button onClick={() => router.push('/homepage')}>Home</button></li>
-        <li><button onClick={() => router.push('/profile')}>Portfolio</button></li>
+        <li><button onClick={() => router.push('/portfolio')}>Portfolio</button></li>
         <li><button onClick={() => router.push('/appointments')}>Clients</button></li>
         <li><button onClick={() => router.push('/helppage')}>Help</button></li>
-        <li><button onClick={() => router.push('/logout')}>Log Out</button></li>
+        <li><button onClick={handlelogout}>Log Out</button></li>
       </ul>
     </nav>
   );
