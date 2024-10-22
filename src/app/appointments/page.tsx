6908a4d018f7b359aa1a9eade687a8e3011c5a55
Ragad Alhaddi <hl6756@wayne.edu>
2024-10-22 01:27:59 -0400

    Fixing navbar directories and creating help page and appointment page

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -7,10 +7,12 @@ import Navbar from '../components/navigationbar';
 
 const Appointment = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-50">
-          <Navbar />
-            <h1 className="text-3xl font-bold">This is the Appointments Page</h1>
-        </div>
+      <div className="min-h-screen bg-gray">
+      <Navbar />
+      <div className="flex items-start justify-start p-4">
+          <h1 className="text-2xl font-bold">Appointments</h1>
+      </div>
+  </div>
     );
 };
 export default Appointment;
