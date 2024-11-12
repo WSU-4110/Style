479d8cd380bc13774563f1d639f6d7259099bcaa
Sapana Pandey <pandeysap2003@gmail.com>
2024-11-12 00:38:37 -0500

    Merged Search and Category page

--- Backend/category/artcategories/views.py
+++ Backend/category/artcategories/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from .models import Type
+from .models import Type, Category
+from django.db.models import Q
 
-# Create your views here.
 def home(request):
-    types = Type.objects.all()
-    return render(request, 'home.html', {'types':types})
+    query = request.GET.get('q')  # get the search query
+    if query:
+        types = Type.objects.filter(
+            Q(name__icontains=query) | 
+            Q(category__name__icontains=query)  # search by type name or category name
+        )
+    else:
+        types = Type.objects.all()
+    return render(request, 'home.html', {'types': types, 'query': query})
