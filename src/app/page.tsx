ef2df1e203adf1e0793e6882777dde58e2b7cc11
nawalr <168575969+nawalragih@users.noreply.github.com>
2024-12-02 11:54:53 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import artistImage from '../public/artist_barber.jpg';
-import customerImage from '../public/customer.jpg';
+import artistImage from '/artist_barber.jpg';
+import customerImage from '/customer.jpg';
 import Image from 'next/image';
 
 export default function LandingPage() {
